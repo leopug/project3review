7cf47122f54a09cdc933d66946cd4071f377e01d
leonardo maia pugliese <leo.pug@hotmail.com>
2019-12-19 22:31:39 -0300

    commit 2

--- project3review/project3review/ViewController.swift
+++ project3review/project3review/ViewController.swift
@@ -1,20 +1,38 @@
-//
-//  ViewController.swift
-//  project3review
-//
-//  Created by Ana Caroline de Souza on 19/12/19.
-//  Copyright © 2019 Ana e Leo Corp. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var countries =  [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Banderolas Great!"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        countries += ["estonia","france","germany","ireland","italy","monaco","nigeria",
+        "poland","russia","spain","uk","us"]
+        
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "DetailFlag") as? FlagDetailViewController {
+            vc.selectedImage = countries[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    
 }
 
