71d2d1d4b3e9cbd2d8da6c489f25104141952a90
leonardo maia pugliese <leo.pug@hotmail.com>
2019-12-19 22:31:31 -0300

    initial commit

---
+++ project3review/project3review/FlagDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FlagDetailViewController.swift
+//  project3review
+//
+//  Created by Ana Caroline de Souza on 19/12/19.
+//  Copyright © 2019 Ana e Leo Corp. All rights reserved.
+//
+
+import UIKit
+
+class FlagDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ project3review/project3review/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project3review
+//
+//  Created by Ana Caroline de Souza on 19/12/19.
+//  Copyright © 2019 Ana e Leo Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
