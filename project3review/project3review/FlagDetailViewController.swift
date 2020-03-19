d3d1f0be1249822274c4dcb3747f4a5a48b7bf47
leonardo maia pugliese <leo.pug@hotmail.com>
2020-03-19 08:45:56 -0300

    challenge of 89th day hacking with swift

--- project3review/project3review/FlagDetailViewController.swift
+++ project3review/project3review/FlagDetailViewController.swift
@@ -25,13 +25,35 @@ class FlagDetailViewController: UIViewController {
     
     @objc func shareTapped(){
         
-        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else
+        guard let realImage = imageView.image else {return}
+        
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 500 , height: 500))
+        
+        let imageToShare = renderer.image { ctx in
+            
+            realImage.draw(at: CGPoint(x: 100, y: 100))
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 32),
+                .paragraphStyle: paragraphStyle
+            ]
+
+            let string = "The meme generator is real"
+
+            let attributedString = NSAttributedString(string: string, attributes: attrs)
+
+            attributedString.draw(with: CGRect(x: 100, y: 100, width: imageView.frame.width/2, height: imageView.frame.height/2), options: .usesLineFragmentOrigin, context: nil)
+        }
+        
+        guard let image = imageToShare.jpegData(compressionQuality: 0.8) else
         {
             print("no image found")
             return
         }
         
-        let vc = UIActivityViewController(activityItems: [image,selectedImage!], applicationActivities: [])
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
         vc.popoverPresentationController?.barButtonItem =
             navigationItem.rightBarButtonItem
         present(vc, animated: true)
